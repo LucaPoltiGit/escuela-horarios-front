b685824200e9acd68830829dceb08283e34bfe63
LucaPoltiGit <luca.tobias.polti@gmail.com>
2026-08-17 21:15:51 -0300

    feat: conectar routing anidado de escuelas en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NavBar } from './components/common/NavBar'
+import { APP_TEXT } from './constants/text'
 import { ROUTES } from './constants/routes'
 import HealthPage from './pages/HealthPage'
 import EscuelasPage from './pages/EscuelasPage'
+import EscuelaDetallePage from './pages/EscuelaDetallePage'
+import EnConstruccionPage from './pages/EnConstruccionPage'
+
+const SECCIONES = APP_TEXT.escuelas.secciones
 
 function App() {
   return (
@@ -11,6 +16,27 @@ function App() {
       <Routes>
         <Route path={ROUTES.salud} element={<HealthPage />} />
         <Route path={ROUTES.escuelas} element={<EscuelasPage />} />
+        <Route path={ROUTES.escuelaDetalle} element={<EscuelaDetallePage />} />
+        <Route
+          path={ROUTES.escuelaBloques}
+          element={<EnConstruccionPage nombreSeccion={SECCIONES.bloques} />}
+        />
+        <Route
+          path={ROUTES.escuelaGrados}
+          element={<EnConstruccionPage nombreSeccion={SECCIONES.grados} />}
+        />
+        <Route
+          path={ROUTES.escuelaMaterias}
+          element={<EnConstruccionPage nombreSeccion={SECCIONES.materias} />}
+        />
+        <Route
+          path={ROUTES.escuelaDocentes}
+          element={<EnConstruccionPage nombreSeccion={SECCIONES.docentes} />}
+        />
+        <Route
+          path={ROUTES.escuelaGenerarHorario}
+          element={<EnConstruccionPage nombreSeccion={SECCIONES.generarHorario} />}
+        />
       </Routes>
     </BrowserRouter>
   )
