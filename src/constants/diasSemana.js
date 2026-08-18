4ab775ff9a29e79f8656ada5380a58d49bb130ef
LucaPoltiGit <luca.tobias.polti@gmail.com>
2026-08-17 21:48:09 -0300

    feat: agregar constante generica de dias de la semana

---
+++ src/constants/diasSemana.js
@@ -0,0 +1,7 @@
+export const DIAS_SEMANA = [
+  { value: 1, label: 'Lunes' },
+  { value: 2, label: 'Martes' },
+  { value: 3, label: 'Miércoles' },
+  { value: 4, label: 'Jueves' },
+  { value: 5, label: 'Viernes' },
+]
